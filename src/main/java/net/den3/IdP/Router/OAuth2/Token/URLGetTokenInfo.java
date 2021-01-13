bf3c1ad07da0309a137e6af30c146d4645884a5f
Kengo Wada <msf.1192@gmail.com>
2021-01-13 13:48:35 +0900

    first commit

---
+++ src/main/java/net/den3/IdP/Router/OAuth2/Token/URLGetTokenInfo.java
@@ -0,0 +1,37 @@
+package net.den3.IdP.Router.OAuth2.Token;
+
+import net.den3.IdP.Entity.Account.IAccount;
+import net.den3.IdP.Entity.Service.IService;
+import net.den3.IdP.Entity.Service.ServicePermission;
+import net.den3.IdP.Store.Account.IAccountStore;
+import net.den3.IdP.Store.Auth.IAccessTokenStore;
+import net.den3.IdP.Store.Auth.ILoginTokenStore;
+import net.den3.IdP.Store.Service.IServiceStore;
+import net.den3.IdP.Util.MapBuilder;
+import net.den3.IdP.Util.StatusCode;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class URLGetTokenInfo {
+    public static void mainFlow(io.javalin.http.Context ctx){
+        String uuid = ILoginTokenStore.getInstance().getAccountUUID(Optional.ofNullable(ctx.header("authorization")).orElse("")).orElse("");
+        Optional<IAccount> account = IAccountStore.getInstance().getAccountByUUID(uuid);
+        if(!account.isPresent()){
+            ctx.status(StatusCode.Unauthorized.code());
+            return;
+        }
+        ctx.json(IAccessTokenStore.getInstance().getTokenByAccountUUID(uuid).stream().map(a->{
+            MapBuilder builder = MapBuilder.New();
+            Optional<IService> service = IServiceStore.getInstance().getService(a.getClientID());
+            if(!service.isPresent()){
+                return builder.build();
+            }
+            builder.put("icon_url",service.get().getServiceIconURL())
+                   .put("service_name",service.get().getServiceName())
+                   .put("permissions", ServicePermission.convertFromScope(a.getScope()))
+                   .put("id",a.getUUID());
+            return builder.build();
+        }).collect(Collectors.toList()));
+    }
+}
